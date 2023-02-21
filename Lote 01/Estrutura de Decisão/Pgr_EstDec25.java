3925b8d9b65a2dde921394c94098959a7637ca22
Bibis <severianobianca@hotmail.com>
2023-02-20 21:42:33 -0300

    Estruturas de Decisão

--- Lote 01/Estrutura de Decisão/Pgr_EstDec25.java
+++ Lote 01/Estrutura de Decisão/Pgr_EstDec25.java
@@ -2,14 +2,42 @@
 Exercício 25: Receba a hora de início e de final de um jogo (HH,MM) sabendo que o 
 tempo máximo é de 24 horas e pode começar num dia e terminar noutro.
 
-Programadora: Bianca S.
+Programadoura: Bianca S.
 
 Data: 19/02/2023
 */
 
-
+import javax.swing.JOptionPane;
 public class Pgr_EstDec25 {
     public static void main (String args []){
+        int Hora_Inicio, Hora_Fim, HoraD, Minuto_Inicio, Minuto_Fim, MinutoD, controle;
+       
+        Hora_Inicio = Integer.parseInt(JOptionPane.showInputDialog("Hora de início do jogo: "));
+        Minuto_Inicio = Integer.parseInt(JOptionPane.showInputDialog("Minutos de início do jogo: "));
+        Hora_Fim = Integer.parseInt(JOptionPane.showInputDialog("Hora do final do jogo: "));
+        Minuto_Fim = Integer.parseInt(JOptionPane.showInputDialog("Minuto do final do jogo: "));
         
+        // calculando horas
+            if (Hora_Inicio >= Hora_Fim){
+                HoraD = (24 - Hora_Inicio)+ Hora_Fim; 
+            } else {
+                HoraD = Hora_Fim - Hora_Inicio;
+            }
+
+        //calculando minutos
+            if (Minuto_Inicio > Minuto_Fim){
+                MinutoD = (60 - Minuto_Inicio) + Minuto_Fim;
+                HoraD = HoraD - 1;
+            } else {
+                MinutoD = Minuto_Fim - Minuto_Inicio;
+            }
+            
+        // apresentando resultados
+            if (HoraD > 24){
+                JOptionPane.showMessageDialog(null, "Partida não pode ter mais que 24h");
+            } else {
+                JOptionPane.showMessageDialog(null, "Jogo durou " + HoraD + ":" + MinutoD);
+            }
+            
     }
 }
--- Lote 01/Estrutura de Decisão/Pgr_EstDec28.java
+++ Lote 01/Estrutura de Decisão/Pgr_EstDec28.java
@@ -1,7 +1,49 @@
+/*
+Exercício 28: Receba o preço atual e a média mensal de um produto. Calcule e mostre o 
+novo preço sabendo que:
+Venda Mensal Preço Atual Preço Novo
+< 500 < 30 + 10%
+>= 500 e < 1000 >= 30 e < 80 +15%
+>= 1000 >= 80 - 5%
+Obs.: para outras condições, preço novo será igual ao preço atual
 
+Programadora: Bianca S.
+
+Data: 19/02/2023
+*/
+import javax.swing.JOptionPane;
 
 public class Pgr_EstDec28 {
     public static void main (String args[]){
+        double precoProduto, mediaMensal, novoPreco;
         
+        precoProduto = Double.parseDouble(JOptionPane.showInputDialog("Digite o preço do produto: R$"));
+        mediaMensal = Double.parseDouble(JOptionPane.showInputDialog("Digite a média mensal de vendas: "));
+        
+        if (precoProduto >=30){ // Preço maior ou igual a 30
+ 
+            if (precoProduto < 80){ // entre 30 e 79
+                if (mediaMensal >= 500 && mediaMensal < 1000) { // condição 2
+                    novoPreco = precoProduto + (precoProduto * 0.15);
+                } else { // exceção 2
+                    novoPreco = precoProduto;
+                }
+            } else { // maior ou igual a 80
+                if (mediaMensal >= 1000){ //condição 3
+                    novoPreco = precoProduto - (precoProduto * 0.05);
+                } else { // exceção 3
+                    novoPreco = precoProduto;
+                }
+            }          
+        } else { // Preço menor que 30
+            
+            if (mediaMensal < 500){
+                novoPreco = (precoProduto + (precoProduto * 0.1));
+            } else {
+                novoPreco = precoProduto;
+            }
+        }
+     
+        JOptionPane.showMessageDialog(null, "Novo preço do produto é: R$" + novoPreco);
     }
 }
